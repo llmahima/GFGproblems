d6e124bd653b1cf420ed58631036c13d339d2c5a
Mahima Nishad <126251630+llmahima@users.noreply.github.com>
2024-06-22 00:45:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Extract the Number from the String/extract-the-number-from-the-string.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+import java.util.regex.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S = read.readLine();
+            Solution ob = new Solution();
+            System.out.println(ob.ExtractNumber(S));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+
+class Solution {
+    long ExtractNumber(String s) {
+        // code here
+        
+        boolean space = true;
+        
+        long num = 0;
+        long mx = 0;
+        
+        for(char c: s.toCharArray()){
+            int ascii = (int)c;
+            
+            if(ascii >= 48 && ascii <= 56 && space){
+                num = num * 10 + (c - '0');
+            }else if(ascii == 57){ // 9 ascii value is 57
+                space = false;
+                mx = Math.max(num, mx);
+                num = 0;
+            }else if(ascii == 32){ // space ascii value is 32
+                mx = Math.max(num, mx);
+                space = true;
+                num = 0;
+            }
+        }
+        
+        mx = Math.max(mx, num);
+        
+        return mx == 0 ? -1 : mx;
+    }
+}
