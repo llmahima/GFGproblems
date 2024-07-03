ca9a5d24d8f8ccaa2166dadc480b9932485b1ec1
Mahima Nishad <126251630+llmahima@users.noreply.github.com>
2024-07-03 12:49:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Remove all occurences of duplicates in a linked list/remove-all-occurences-of-duplicates-in-a-linked-list.java
@@ -0,0 +1,89 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+class GFG {
+    Node head;
+    Node tail;
+
+    public void addToTheLast(Node node) {
+        if (head == null) {
+            head = node;
+            tail = node;
+        } else {
+            tail.next = node;
+            tail = node;
+        }
+    }
+
+    void printList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    /* Drier program to test above functions */
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0) {
+            int N = sc.nextInt();
+            GFG llist = new GFG();
+            int a1 = sc.nextInt();
+            Node head = new Node(a1);
+            llist.addToTheLast(head);
+            for (int i = 1; i < N; i++) {
+                int a = sc.nextInt();
+                llist.addToTheLast(new Node(a));
+            }
+
+            Solution ob = new Solution();
+            head = ob.removeAllDuplicates(llist.head);
+            llist.printList(head);
+
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public Node removeAllDuplicates(Node head) {
+          Node result = null;
+        Node curr = null;
+        int removingData = -1;
+        
+        while(head!=null){
+            if(head.next !=null && head.data == head.next.data){
+                removingData = head.data;
+            }else{
+                if(head.data != removingData){
+                    if(curr == null){
+                        curr = head;
+                        result = curr;
+                    }  
+                    else{
+                        curr.next = head;
+                        curr = curr.next;
+                    }
+                }
+            }
+            head = head.next;
+        }
+        if(curr.next != null)   curr.next = null;
+        return result;
+    }
+}
